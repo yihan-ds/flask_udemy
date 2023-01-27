cdc9f6d3c6baaff0a2ce49b0661c6c88bb3fa80a
yihan-ds <hiewyihan.studies@gmail.com>
2023-01-27 22:02:55 +0800

    add more route

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return "Welcome Human!"
+
+@app.route('/helloworld')
 def welcome():
     return "Hello World. This is my first Flask app. Yay!"
 
